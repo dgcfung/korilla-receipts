6886b5153a7cf0e684caff355eac704c7076f505
B Legit <fungd@Donalds-MacBook-Air.local>
2020-01-14 19:45:50 -0500

    all requirements complete

--- korrila/src/data/Receipt.jsx
+++ korrila/src/data/Receipt.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component}from 'react';
+
 
 // class App extends React.Component {
 //     constructor(props){
@@ -19,21 +20,42 @@ import React from 'react';
 //     );
 //   }
 
-function Receipt(props) {
+class Receipt extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state= {
+            paid: props.receipt.paid
+        }
+        this.paid = this.paid.bind(this)
+    }
+
+    paid(e){
+        console.log('hello')
+        this.setState({
+            paid: true
+        })
+        
+    }
+
+    render(){
     return (
         <div>
         <div>
-            <h1>{props.receipt.person}</h1>
-            <h4>{props.receipt.order.main}</h4>
-            <h4>{props.receipt.order.protein}</h4>
-            <h4>{props.receipt.order.rice}</h4>
-            <h4>{props.receipt.order.sauce}</h4>
-            <h4>{props.receipt.order.drink}</h4>
-            <h4>{props.receipt.order.cost}</h4>
-            <h3>{props.receipt.paid.toString()}</h3>
+            <h1>{this.props.receipt.person}</h1>
+            <h4>{this.props.receipt.order.main}</h4>
+            <h4>{this.props.receipt.order.protein}</h4>
+            <h4>{this.props.receipt.order.rice}</h4>
+            <h4>{this.props.receipt.order.sauce}</h4>
+            <h4>{this.props.receipt.order.drink}</h4>
+            <h4>Total: ${this.props.receipt.order.cost}</h4>
+            <h4>Tender: {this.state.paid.toString()}</h4>
+            <button onClick={this.paid}>Pay Now</button>
         </div>
         </div>
     )
+    }
 }
 
 export default Receipt
